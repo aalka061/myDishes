3ed0996bd13a7cd10cb392b73b283121957773c1
abdullah alkaf <aalkaf@smatbuildingapps.com>
2021-01-03 15:01:50 -0500

    creating categories screen 157-158

---
+++ lib/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_dishes/category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          ('DeliMeal'),
+        ),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(
+                e.title,
+                e.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
