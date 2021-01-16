58d42f673dab3084b75254b76d15523f2aacb033
abdullah alkaf <aalkaf@smatbuildingapps.com>
2021-01-16 13:20:12 -0500

    added bottom bar instead of topbar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -7,7 +7,27 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
+// bottom bar
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorites',
+    },
+  ];
+
+  int _selectedIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -15,27 +35,67 @@ class _TabsScreenState extends State<TabsScreen> {
       initialIndex: 0,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              )
-            ],
-          ),
+          title: Text(_pages[_selectedIndex]['title']),
         ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
+        body: _pages[_selectedIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          backgroundColor: Theme.of(context).primaryColor,
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(
+                Icons.category,
+              ),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(
+                Icons.category,
+              ),
+              label: 'Favorites',
+            ),
           ],
         ),
       ),
     );
   }
 }
+// top bar
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       initialIndex: 0,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Meals'),
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 icon: Icon(Icons.category),
+//                 text: 'Categories',
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.star),
+//                 text: 'Favorites',
+//               )
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             CategoriesScreen(),
+//             FavoritesScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
